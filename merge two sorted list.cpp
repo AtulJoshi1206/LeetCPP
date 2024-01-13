1f0f6c50f5a89a8cf1c7073c48a1351f14f67d87
Atul Joshi <120785343+AtulJoshi1206@users.noreply.github.com>
2024-01-13 20:57:31 +0530

    Create merge two sorted list.cpp

---
+++ merge two sorted list.cpp
@@ -0,0 +1,65 @@
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* merge(ListNode* l1,ListNode* l2){
+        if(l1==NULL){
+            return l2;
+        }
+        if(l2==NULL){
+            return l1;
+        }
+        if(l1->val<l2->val){
+            l1->next = merge(l1->next,l2);
+            return l1;
+        }else{
+            l2->next = merge(l1,l2->next);
+            return l2;
+        }
+    }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL)return list2;
+        if(list2==NULL)return list1;
+        ListNode* tmp = new ListNode(-1000);
+        tmp->next = list1;
+        merge(tmp,list2);
+        return tmp->next;
+    }
+};
