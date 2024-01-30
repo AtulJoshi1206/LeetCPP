081e415774e83834ab738aa5878600be4bc51b44
Atul Joshi <120785343+AtulJoshi1206@users.noreply.github.com>
2024-01-30 21:06:14 +0530

    Update Determine if string are alike.cpp

--- Determine if string are alike.cpp
+++ Determine if string are alike.cpp
@@ -2,7 +2,7 @@ You are given a string s of even length. Split this string into two halves of eq
 
 Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
 
-Return true if a and b are alike. Otherwise, return false.
+Return  true if a and b are alike. Otherwise, return false.
 
  
 
